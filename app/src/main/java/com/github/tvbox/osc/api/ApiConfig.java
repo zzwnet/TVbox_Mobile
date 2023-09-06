ea3dbbcfe82d5d2365cce56b2c0c85d77f609692
zzwnet <72076459+zzwnet@users.noreply.github.com>
2023-09-06 19:54:05 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://159.75.9.236/tv/ymza.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
