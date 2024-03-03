8f9d1c423140829627fcc3779a4db59181c4ccff
zzwnet <72076459+zzwnet@users.noreply.github.com>
2024-03-04 01:19:55 +0800

    Update ApiConfig.java
    撒旦撒

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://jihulab.com/4kjidi/4kjidi1/-/raw/main/Achaoyue.json");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http:/太硬.top/tv");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
