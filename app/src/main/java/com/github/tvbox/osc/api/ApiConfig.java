8311db3ff727db8ba65e7c668e366cc81ad53c7c
zzwnet <72076459+zzwnet@users.noreply.github.com>
2024-03-04 01:27:17 +0800

    Update ApiConfig.java
    阿斯顿

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "http:/太硬.top/tv");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://饭太硬.top/tv");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
